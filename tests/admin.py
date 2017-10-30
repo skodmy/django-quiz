e673b78508d8f821b17affacadd037c004aa49b6
Skochynskiy Dmytro <skodmy@gmail.com>
2017-10-30 14:00:32 +0200

    registered models

--- tests/admin.py
+++ tests/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import SUBJECT_MODEL, TimeConstraint, MarkConstraint, Test, Question, Answer
 
-# Register your models here.
+if not admin.site.is_registered(SUBJECT_MODEL):
+    admin.site.register(SUBJECT_MODEL)
+
+admin.site.register(TimeConstraint)
+admin.site.register(MarkConstraint)
+admin.site.register(Test)
+admin.site.register(Question)
+admin.site.register(Answer)
